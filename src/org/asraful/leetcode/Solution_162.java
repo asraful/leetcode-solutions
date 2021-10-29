d3e481150c166f62e34effcf007b5dc017ecb849
Asraful Forhad Chowdhury <asraful.cse@gmail.com>
2021-10-29 19:28:13 +0600

    leetcode solution,162. Find Peak Element

---
+++ src/org/asraful/leetcode/Solution_162.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/*
+    https://github.com/asraful
+    LeetCode problem title : 162. Find Peak Element
+ */
+
+public class Solution_162 {
+    public int findPeakElement(int[] arr) {
+        return peakIndexInMountainArray(arr, 0, arr.length - 1);
+    }
+
+    static int peakIndexInMountainArray(int arr[], int l, int r) {
+        while (l < r) {
+            //find medium
+            int m = l + (r - l) / 2;
+
+            if (arr[m] > arr[m + 1]) {// decreasing slope
+                r = m;
+            } else if (arr[m] < arr[m + 1]) { //increasing slope
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+}
