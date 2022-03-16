25fa515db8a6ec418ab8407f911024fa88424d52
Asraful Forhad Chowdhury <asraful.cse@gmail.com>
2022-03-16 23:34:37 +0600

    Find Numbers with Even Number of Digits

--- src/org/asraful/leetcode/SolutionEvenNumberofDigits.java
+++ src/org/asraful/leetcode/SolutionEvenNumberofDigits.java
@@ -1,4 +1,23 @@
 package leetcode;
 
 public class SolutionEvenNumberofDigits {
+
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        int evenCount = 0;
+        for (int i = 0; i <= nums.length - 1; i++) {
+
+            int temp = nums[i];
+            while (temp != 0) {
+                temp = temp / 10;
+                ++count;
+            }
+            System.out.println(count);
+            if (count % 2 == 0) {
+                evenCount++;
+            }
+            count = 0;
+        }
+        return evenCount;
+    }
 }
--- src/org/asraful/leetcode/SolutionTest.java
+++ src/org/asraful/leetcode/SolutionTest.java
@@ -4,9 +4,9 @@ public class SolutionTest {
     public static void main(String args[]) {
 //        int nums[] = {1, 1, 0, 1, 1, 1};
 
-        int nums[] = {1, 1, 1, 0, 1, 1, 1, 1, 0, 1};
+        int nums[] = {555, 901, 482, 1771};
 
-       int res =  new SolutionConsecutiveOnes().findMaxConsecutiveOnes(nums);
+        int res = new SolutionEvenNumberofDigits().findNumbers(nums);
 
         System.out.println(res);
     }
