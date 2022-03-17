08ea0bd70633067bcc26b1d3999676a594609bbb
Asraful Forhad Chowdhury <asraful.cse@gmail.com>
2022-03-18 02:23:06 +0600

    TwoPointers,Array,Solution_88

--- src/org/asraful/leetcode/SolutionTest.java
+++ src/org/asraful/leetcode/SolutionTest.java
@@ -1,7 +1,14 @@
 package leetcode;
 
+import array.MergeSortedArray;
+
 public class SolutionTest {
     public static void main(String args[]) {
+        int nums1[] = {2,0};
+        int m = 1;
+        int nums2[] = {1};
+        int n = 1;
+        new MergeSortedArray().merge(nums1, m, nums2, n);
 
 
     }
