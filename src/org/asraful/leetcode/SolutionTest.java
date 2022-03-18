09f431de30a1de8fe3174433eced2d7053fc4554
Asraful Forhad Chowdhury <asraful.cse@gmail.com>
2022-03-18 16:42:02 +0600

    check double exisit, array

--- src/org/asraful/leetcode/SolutionTest.java
+++ src/org/asraful/leetcode/SolutionTest.java
@@ -1,16 +1,16 @@
 package leetcode;
 
+import array.DoubleExist;
 import array.MergeSortedArray;
 import array.RemoveDuplicatesfromSortedArray;
 import array.RemoveElement;
 
 public class SolutionTest {
     public static void main(String args[]) {
-        int nums1[] = {1,1,2,2};
+        int nums1[] = {-20, 8, -6, -14, 0, -19, 14, 4};
 
 
-
-        int k = new RemoveDuplicatesfromSortedArray().removeDuplicates(nums1);
+        boolean k = new DoubleExist().checkIfExist(nums1);
         System.out.println(k);
 
 
