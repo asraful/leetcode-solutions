03e170985ef21868cb95cb4a4beb6b2d5ace1f35
Asraful Forhad Chowdhury <asraful.cse@gmail.com>
2022-03-21 03:21:14 +0600

    array,two pointers,in-place operation

---
+++ src/org/asraful/array/Solution_283.java
@@ -0,0 +1,19 @@
+package array;
+
+public class Solution_283 {
+    // two pointer
+    public void moveZeroes(int[] nums) {
+
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+
+        while (j < nums.length) {
+            nums[j++] = 0;
+        }
+
+    }
+}
