3489015603e6978b3bfa63cce6a9e9c0026f0771
Asraful Forhad Chowdhury <asraful.chowdhury@tuni.fi>
2024-01-04 06:50:18 +0200

    125 Two Pointer Approach

--- src/org/asraful/leetcode/Solution_125.java
+++ src/org/asraful/leetcode/Solution_125.java
@@ -11,4 +11,23 @@ public class Solution_125 {
 
         return false;
     }
+
+    //two pointer approach
+    public boolean isPalindrome_(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l)))
+                ++l;
+            while (l < r && !Character.isLetterOrDigit(s.charAt(r)))
+                --r;
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r)))
+                return false;
+            ++l;
+            --r;
+        }
+
+        return true;
+    }
 }
