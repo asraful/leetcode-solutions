762e8897559bf335cae3b9d99dda87b2d16b1012
Asraful Forhad Chowdhury <asraful.chowdhury@tuni.fi>
2024-01-05 04:19:04 +0200

    167. Two Sum II - Input Array Is Sorted

---
+++ src/org/asraful/leetcode/Solution_167.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class Solution_167 {
+
+    // two pointer approach
+    public int[] twoSum(int[] numbers, int target) {
+
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while(numbers[left] + numbers[right] != target) {
+            if(numbers[left] + numbers[right] < target) {
+                left++;
+            }else {
+                right--;
+            }
+        }
+        return new int[] {left + 1, right + 1};
+    }
+}
